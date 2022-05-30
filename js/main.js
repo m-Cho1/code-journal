4149eaa412ea7da4afd9b2563b15eaf0ce7adad8
m-Cho1 <mturtle051@gmail.com>
2022-05-30 20:26:30 +0000

    add function for submit event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 var $photoUrl = document.querySelector('#photo-url');
+var $title = document.querySelector('#title');
+var $notes = document.querySelector('#notes');
 var $imgSrc = document.querySelector('.placeholder-img');
+var $form = document.querySelector('#form-entry');
 
 $photoUrl.addEventListener('input', function (event) {
   $imgSrc.setAttribute('src', $photoUrl.value);
 });
+
+$form.addEventListener('submit', submitEventFunction);
+
+function submitEventFunction(event) {
+  event.preventDefault();
+  var newEntryData = {
+    title: $title.value,
+    photoUrl: $photoUrl.value,
+    notes: $notes.value,
+    nextEntryId: data.nextEntryId
+  };
+  data.nextEntryId++;
+  data.entries.unshift(newEntryData);
+  $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
+}
