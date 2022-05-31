b175bcfcdf9034e86e4df7480e631f85e81de767
m-Cho1 <mturtle051@gmail.com>
2022-05-31 01:10:33 +0000

    define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the html

--- js/main.js
+++ js/main.js
@@ -23,3 +23,42 @@ function submitEventFunction(event) {
   $imgSrc.setAttribute('src', 'images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+function createEntry(entry) {
+  var listEl = document.createElement('li');
+  listEl.setAttribute('class', 'style-list-entry');
+
+  var containerDiv = document.createElement('div');
+  containerDiv.setAttribute('class', 'container');
+  listEl.appendChild(containerDiv);
+
+  var rowDiv = document.createElement('div');
+  rowDiv.setAttribute('class', 'row');
+  containerDiv.appendChild(rowDiv);
+
+  var columnDiv1 = document.createElement('div');
+  columnDiv1.setAttribute('class', 'column-half');
+  rowDiv.appendChild(columnDiv1);
+
+  var imgEl = document.createElement('img');
+  imgEl.setAttribute('class', 'style-image-entry');
+  imgEl.setAttribute('src', $photoUrl.value);
+  imgEl.setAttribute('alt', 'image');
+  columnDiv1.appendChild(imgEl);
+
+  var columnDiv2 = document.createElement('div');
+  columnDiv2.setAttribute('class', 'column-half font line-height');
+  rowDiv.appendChild(columnDiv2);
+
+  var h2Element = document.createElement('h2');
+  h2Element.setAttribute('class', 'title-margin-top');
+  h2Element.textContent = $title.value;
+  columnDiv2.appendChild(h2Element);
+
+  var pElement = document.createElement('p');
+  pElement.textContent = $notes.value;
+  columnDiv2.appendChild(pElement);
+
+  return listEl;
+
+}
